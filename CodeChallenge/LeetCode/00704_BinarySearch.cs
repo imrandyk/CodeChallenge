b3ac37de5b88ed8455ff881f08e448c864b2cab6
imrandyk <imrandyk@gmail.com>
2022-08-28 17:23:23 -0500

    been so long since I did this

---
+++ CodeChallenge/LeetCode/00704_BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeChallenge.LeetCode
+{
+    partial class LeetCode
+    {
+        public int Search(int[] nums, int target)
+        {
+            var step = (nums.Length -0) / 2;
+            var index = 0 + step;
+
+            if (nums.Length == 1 && nums[0] == target)
+                return 0;
+
+            for (int i = 0; i <= nums.Length / 2; i++)
+            {
+                if (index < 0 || index >= nums.Length)
+                    return -1;
+
+                if (nums[index] == target)
+                    return index;
+
+                step /= 2;
+                if (step == 0)
+                    step++;
+                index += nums[index] > target ? -1 * step : step;
+            }
+            return -1;
+        }
+    }
+}
